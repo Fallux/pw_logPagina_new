697e1f924ea9b0e36d6d2c897a4cda8213fb455a
Roemer Naber <200611@student.glu.nl>
2021-09-09 17:22:36 +0200

    onfocus kleur aanpassing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,25 @@
 console.log("hello world");
+let gebruiksnaam = document.getElementById("input1");
+let wachtwoord = document.getElementById("input2");
 //email-adres input
 function focusFunction1() {
     // Focus = Changes the background color of input to pink
-    document.getElementById("input1").style.background = "pink";
-    document.getElementById("input1").style.color = "black";
+    gebruiksnaam.style.background = "pink";
+    gebruiksnaam.style.color = "black";
   }
   function blurFunction1() {
-    // Focus = Changes the background color of input to purple
-    document.getElementById("input1").style.background = "purple";
-    document.getElementById("input1").style.color = "white";
+    // Focus = Changes the background color of input to lightsteelblue
+    gebruiksnaam.style.background = "lightsteelblue";
+    gebruiksnaam.style.color = "white";
   }
 //   wachtwoord input
   function focusFunction2() {
     // Focus = Changes the background color of input to pink
-    document.getElementById("input2").style.background = "pink";
-    document.getElementById("input2").style.color = "black";
+    wachtwoord.style.background = "pink";
+    wachtwoord.style.color = "black";
   }
   function blurFunction2() {
-    // Focus = Changes the background color of input to purple
-    document.getElementById("input2").style.background = "purple";
-    document.getElementById("input2").style.color = "white";
+    // Focus = Changes the background color of input to lightsteelblue
+    wachtwoord.style.background = "lightsteelblue";
+    wachtwoord.style.color = "white";
   }
