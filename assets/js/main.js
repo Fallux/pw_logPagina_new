8267c507d039e3c44a97903b988bf80027c4e029
Roemer Naber <200611@student.glu.nl>
2021-09-09 17:10:42 +0200

    onfocus en blur toegevoegd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
 console.log("hello world");
+//email-adres input
+function focusFunction1() {
+    // Focus = Changes the background color of input to pink
+    document.getElementById("input1").style.background = "pink";
+    document.getElementById("input1").style.color = "black";
+  }
+  function blurFunction1() {
+    // Focus = Changes the background color of input to purple
+    document.getElementById("input1").style.background = "purple";
+    document.getElementById("input1").style.color = "white";
+  }
+//   wachtwoord input
+  function focusFunction2() {
+    // Focus = Changes the background color of input to pink
+    document.getElementById("input2").style.background = "pink";
+    document.getElementById("input2").style.color = "black";
+  }
+  function blurFunction2() {
+    // Focus = Changes the background color of input to purple
+    document.getElementById("input2").style.background = "purple";
+    document.getElementById("input2").style.color = "white";
+  }
